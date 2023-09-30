6f520bc9d7f56c1544476a8402e3b62a1b312c20
Layne Silveira <laynepsilveira@gmail.com>
2023-09-30 13:24:22 -0300

    chore: steg module outline

---
+++ src/steg.py
@@ -0,0 +1,13 @@
+import numpy
+
+def hide(img, cipher):
+    # receives the hashed cipher and the img to hide the cipher into
+    pass
+
+def reveal(img):
+    # retrieves the cipher from the given img
+    pass
+
+def bitplane(n, img):
+    # gets the nth bit plane of the given img
+    pass
