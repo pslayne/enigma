dfadc5db5ed0c6f63fbbc0ebdc626977330b99ff
Layne Silveira <laynepsilveira@gmail.com>
2023-09-30 15:55:30 -0300

    feature: updated module crypto

--- src/crypto.py
+++ src/crypto.py
@@ -1,6 +1,4 @@
-import numpy
 from random import randbytes 
-import hashlib
 
 def generate_key(size):
     # generate a random key with the exact size of the msg
@@ -10,31 +8,23 @@ def xor(a, b):
     result_int = int.from_bytes(a, byteorder="big") ^ int.from_bytes(b, byteorder="big")
     return result_int.to_bytes(max(len(a), len(b)), byteorder="big")
 
-
-def max_size(img_shape):
-    # receives the shape of the numpy array that represents the img
-    # calculates the maximum size of the msg this img can hide
-    pass
-
-def hash(cipher, max_size):
-    # hashes the cipher
-    pass
-
 def encrypt(msg):
     try:
         assert type(msg) == type('str')
     except AssertionError:
         print('encrypt function expects a string')
-    # get a key
+    
     key = generate_key(len(msg))
-    # msg xor key
     encrypted_msg = xor(msg.encode(), key)
-    return encrypted_msg, key
-    # returns the combined hash
+    size = (len(key) * 2).to_bytes(4, byteorder='big')
+    print(len(key) * 2)
+    print(size)
+    concat = size + key + encrypted_msg
+    return concat
 
-def decrypt(cipher, key): # remove the key
-    # receives the hash of the cipher msg and the key combined
-    # "dehash" and decipher
-    # returns the plain text
-    return xor(cipher, key)
+def decrypt(cipher, size):
+    key = cipher[:size]
+    msg = cipher[size:]
+    plain = xor(msg, key).decode()
+    return plain
 
