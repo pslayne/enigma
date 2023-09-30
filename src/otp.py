441c3416414a19b5cb6b69b0fe2c19db200ec743
Layne Silveira <laynepsilveira@gmail.com>
2023-09-30 13:16:24 -0300

    chore: otp module outline

---
+++ src/otp.py
@@ -0,0 +1,16 @@
+import numpy 
+
+def generate_key(size):
+    pass
+
+def encrypt(msg, key):
+    # msg xor key
+    # returns the combined hash
+    pass
+
+def decrypt(msg, key):
+    # receives the hash of the cipher msg and the key combined
+    # "dehash" and decipher
+    # returns the plain text
+    pass
+
